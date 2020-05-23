562905143176ae572f5f11b8f9f84fc40e6d6ab7
m.shcherbina <mshcherbina99@gmail.com>
2020-05-23 23:07:47 +0300

    added CSM Lab:Petri system imitiation

---
+++ CSM/Lab5/src/NetworkStatistics.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkStatistics {
+
+    static List<List<StatsEntry>> positionsStats;
+    static double maxTime;
+    static List<ReportEntry> reportFormat;
+
+    static class StatsEntry{
+        double timestamp;
+        int markers;
+
+        StatsEntry(double timestamp, int markers){
+            this.timestamp = timestamp;
+            this.markers = markers;
+        }
+    }
+
+    static class ReportEntry{
+        enum Type{
+            MarkersInTheEndOfSimulation,
+            AverageMarkers
+        }
+
+        Type type;
+        int positionId;
+        String message;
+
+        ReportEntry(Type type, int positionId, String message){
+            this.type = type;
+            this.positionId = positionId;
+            this.message = message;
+        }
+    }
+
+    static void init(int positions, double maxTime){
+        NetworkStatistics.maxTime = maxTime;
+        positionsStats =  new ArrayList<>();
+        for (int i = 0; i < positions; i++){
+            positionsStats.add(new ArrayList<>());
+            positionsStats.get(i).add(new StatsEntry(0, 0));
+        }
+        reportFormat = new ArrayList<>();
+    }
+
+    static void positionChange(Position position){
+        StatsEntry entry = new StatsEntry(Timeline.getTime(), position.getMarkers());
+        positionsStats.get(position.getId()).add(entry);
+    }
+
+    static void addReportEntry(ReportEntry.Type type, int positionId, String message){
+        reportFormat.add(new ReportEntry(type, positionId, message));
+    }
+
+    private static ArrayList<Double> averageMarkers() {
+
+        ArrayList<Double> averageMarkers = new ArrayList<>();
+
+        for (List<StatsEntry> positionStats: positionsStats){
+            double prevTimestamp = 0, prevMarkers = 0, accumulator = 0;
+
+            for (StatsEntry current: positionStats){
+                accumulator += (current.timestamp - prevTimestamp) * prevMarkers;
+                prevMarkers = current.markers;
+                prevTimestamp = current.timestamp;
+            }
+
+            accumulator += (maxTime - prevTimestamp) * prevMarkers;
+            averageMarkers.add(accumulator / maxTime);
+        }
+
+        return averageMarkers;
+    }
+
+
+    public static void printReport(){
+        System.out.println();
+        System.out.println("Simulation results: ");
+
+        List<Double> averageMarkers = averageMarkers();
+
+        for (ReportEntry reportEntry : reportFormat){
+            StringBuilder reportEntryBuilder = new StringBuilder();
+            reportEntryBuilder.append(reportEntry.message).append(": ");
+
+            if (reportEntry.type == ReportEntry.Type.AverageMarkers){
+                reportEntryBuilder.append(String.format("%.2f", averageMarkers.get(reportEntry.positionId)));
+            }
+            if (reportEntry.type == ReportEntry.Type.MarkersInTheEndOfSimulation){
+                List<StatsEntry> statsReportEntryPosition = positionsStats.get(reportEntry.positionId);
+                reportEntryBuilder.append(statsReportEntryPosition.get(statsReportEntryPosition.size() - 1).markers);
+            }
+            System.out.println(reportEntryBuilder.toString());
+        }
+    }
+}
---
+++ CSM/Lab5/src/Transition.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transition {
+    private static int nextId = 0;
+
+    private int id;
+    private int priority = 0;
+    private List<Link> inputs;
+    private List<Link> outputs;
+    private ExponentialRNG delayRNG = null;
+
+    Transition(){
+        this.id = nextId;
+        nextId++;
+        inputs = new ArrayList<>();
+        outputs = new ArrayList<>();
+    }
+
+    void addDelayRNG(double lambda){
+        delayRNG = new ExponentialRNG(lambda);
+    }
+
+    void addInput(Link link){
+       inputs.add(link);
+    }
+
+    void addOutput(Link link){
+        outputs.add(link);
+    }
+
+    void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    int getPriority() {
+        return this.priority;
+    }
+
+    int getId(){
+        return this.id;
+    }
+
+    boolean hasDelay() {
+        return delayRNG != null;
+    }
+
+    void activate() {
+        System.out.println("Transition " + this.getId() + " was activated");
+        inputs.forEach(Link::withdrawMarkers);
+        if (this.hasDelay()) {
+            double delay = delayRNG.next();
+            Timeline.addEvent(new Event(Timeline.getTime() + delay, this));
+        } else {
+            withdraw();
+        }
+    }
+
+    void withdraw(){
+        System.out.println("Transition " + this.getId() + " was freed");
+        outputs.forEach(Link::addMarkers);
+    }
+
+    boolean isSatisfiable() {
+        for (Link link: inputs){
+            if (!link.isSatisfiable()) return false;
+        }
+        return true;
+    }
+
+}
