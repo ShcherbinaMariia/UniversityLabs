8fd8c5cc751b030c772888791eb29745425cacc8
m.shcherbina <mshcherbina99@gmail.com>
2020-05-31 15:57:06 +0300

    lab Parallel Computations: OpenMPI: decrease channels traffic

--- ParallelComputations/OpenMPI_v2/main.cpp
+++ ParallelComputations/OpenMPI_v2/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <mpi.h>
 
-int kCommunicationInterval = 1000;
+int kCommunicationInterval = 10000;
 
 int num_processes, rank;
 int first, last;
@@ -54,26 +54,31 @@ int main(int argc, char* argv[]) {
 	auto start = std::chrono::high_resolution_clock::now();
 
 	Checker* checker = checkerFactory.createChecker();
-	int foundLocal = 0;
+	bool foundLocal = false;
+	int localResult = 0;
 
 	for (unsigned int curr = first; curr <= last; curr++){
 		if (!foundLocal && checker->check(curr)){
-			foundLocal = 1;
-			std::cout << "Result is: " << curr << "\n";
+			foundLocal = true;
+			localResult = curr;
 		}
 
 		if ((curr - first) % kCommunicationInterval == 0){
-			int foundGlobal = 0;
-			MPI_Allreduce(&foundLocal, &foundGlobal, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+			bool foundGlobal = false;
+			MPI_Allreduce(&foundLocal, &foundGlobal, 1, MPI_CXX_BOOL, MPI_LOR, MPI_COMM_WORLD);
 			if (foundGlobal){
 				break;
 			}
 		}
 	}
 
+	int globalResult = 0;
+	MPI_Allreduce(&localResult, &globalResult, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 	if (rank == 0) {
+		std::cout << "Result is: " << globalResult << "\n";
 		std::cout << "Execution time: " << duration.count() << " microseconds\n"; 
 	}
 	MPI_Finalize();
